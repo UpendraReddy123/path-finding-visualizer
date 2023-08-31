58620a09fee75652698c45aad2647feef151a725
S Upendra Reddy <94696595+UpendraReddy123@users.noreply.github.com>
2023-08-31 08:26:05 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,9 @@ import "./Footer.scss";
 const Footer = () => {
     return ( 
         <div className="bg-dark text-center">
-            <a href="https://www.linkedin.com/in/sethuram-s-v-171719194/" className="fa fa-linkedin"></a>
-            <a href="https://github.com/Sethuram52001" className="fa fa-github"></a>
-            <a href="https://sethuram52001.medium.com/" className="fa fa-medium"></a>
+            <a href="https://www.linkedin.com/in/upendra-reddy-s-94b94021a/" className="fa fa-linkedin"></a>
+            <a href="https://github.com/UpendraReddy123" className="fa fa-github"></a>
+            <a href="https://supendrareddy2023.medium.com/" className="fa fa-medium"></a>
         </div>
      );
 }
